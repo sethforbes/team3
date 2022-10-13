f9c49ea80319fa592f0a3de9f9bc4dd26bda2d2e
Brock Tibert <btibert3@gmail.com>
2022-10-13 14:36:17 -0400

    lab3 part 1 updates

--- lab1/app/main.py
+++ lab1/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from starlette.responses import FileResponse 
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
+@app.get("/landing")
+def landing_page():
+    return FileResponse("static/page1.html")
